962a147df8932ffcf1e0731145fc0f1aab2bfd76
Taras Savechko <__it@ILAB3>
2018-10-05 19:04:27 +0300

    Some new changes in webpack

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
-<div id="container">
-{{ message }}
-</div>
+<Sidebar/>
 </template>
 <script>
+import Sidebar from './Components/Sidebar'
+
 export default {
   data () {
     return {
       message: 'dgdgdg'
     }
+  },
+  name: 'App',
+  components: {
+    Sidebar
   }
 }
+
 </script>
---
+++ src/Components/Sidebar.vue
@@ -0,0 +1,43 @@
+<template>
+<div class="sidebar">
+  <div class="sidebar-body">
+    <ul>
+      <li>Home</li>
+      <li>My music</li>
+      <li>Playlists</li>
+      <li>Search</li>
+    </ul>
+  </div>
+</div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      message: 'dgdgdg'
+    }
+  }
+}
+</script>
+<style>
+ul {
+  display: block;
+  list-style-type: disc;
+  margin-block-start: 1em;
+  margin-block-end: 1em;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  padding-inline-start: 0px;  
+}
+ul li {
+  list-style: none;
+}
+.sidebar {
+  min-width: 400px;
+  display: block;
+  height: 100%;
+}
+.sidebar-body {
+  background-color: lightgrey;
+}
+</style>
